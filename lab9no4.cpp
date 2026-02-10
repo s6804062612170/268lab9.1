d0811c1d9d7fc620dd3de6205aa9843e3622d9bc
s6804062612170 <s6804062612170@email.kmutnb.ac.th>
2026-02-10 14:56:00 +0700

    Commit 2> lab9no4.c:swap array1d

--- lab9no4.cpp
+++ lab9no4.cpp
@@ -2,7 +2,7 @@
 
 //function prototype
 	void swapValue(int *a, int *b, int *c);
-//	void swapArray(int a[], int b[], int size); 
+	void swapArray(int a[], int b[], int size); 
 
 	
 int main() {
@@ -11,6 +11,20 @@ int main() {
 	swapValue(&a ,&b ,&c );
 	printf("After swap function: a= %d, b=%d, c=%d\n", a, b, c);
 	//calling swapArray()
+	int ar1[5] ={1,2,3,4,5};
+	int ar2[5] ={10,20,30,40,50};
+	
+	printf("Before swapArray:\n");
+	for(int i =0;i <5;i++)
+		printf("ar1[%d]=%d ar2[%d]=%d\n",i,ar1[i],i,ar2[i]);
+	
+	swapArray(ar1,ar2,5);
+	
+	printf("After swapArray:\n");
+	for(int i =0;i <5;i++)
+		printf("ar1[%d]=%d ar2[%d]=%d\n",i,ar1[i],i,ar2[i]);
+		
+	return 0;
 }
 void swapValue(int *a, int *b,int *c)
 {
@@ -18,4 +32,14 @@ void swapValue(int *a, int *b,int *c)
 	*c = *a;
 	*a = *b;
 	*b = temp;
+}
+void swapArray(int a[], int b[],int size)
+{
+	int temp;
+	for	(int i =0;i < size;i++)
+	{
+		temp = a[i];
+		a[i] = b[i];
+		b[i] = temp;
+	}
 }
